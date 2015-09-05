327d4234496c3975a55114a1ddae40ec418683c5
USAMI Kenta <tadsan@zonu.me>
2015-09-06 05:14:10 +0900

    Add twm-request-bridge.el

---
+++ twm-request-bridge.el
@@ -0,0 +1,459 @@
+;;; twm-request-bridge.el --- HTTP Client for Emacs
+
+;; Copyright (C) 2009-2015 Tadashi MATSUO
+;;               2007, 2009-2011 Yuto Hayamizu.
+;;               2008 Tsuyoshi CHO
+;;               2014, 2015 Xavier Maillard
+
+;; Author: Tadashi MATSUO <tad@mymail.twin.ne.jp>
+;;	Y. Hayamizu <y.hayamizu@gmail.com>
+;;	Tsuyoshi CHO <Tsuyoshi.CHO+develop@Gmail.com>
+;;	Alberto Garcia <agarcia@igalia.com>
+;;	Xavier Maillard <xavier@maillard.im>
+;; Created: Sep 4, 2007
+;; Version: HEAD
+;; Identity: $Id: f0574b157c4e8ac5e3b25e0385a7131681260343 $
+;; Keywords: http client
+;; URL: https://github.com/zonuexe/twm-request
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+;;; Commentary:
+
+;; twm-request-proxy.el is HTTP Client for Emacs.
+;; This library is drived from twittering-mode. http://github.com/hayamiz/twittering-mode
+
+;;; Code:
+(require 'twm-request-util)
+
+(defvar twm-request-curl-program nil
+  "Cache a result of `twm-request-find-curl-program'.
+DO NOT SET VALUE MANUALLY.")
+(defvar twm-request-curl-program-https-capability nil
+  "Cache a result of `twm-request-start-http-session-curl-https-p'.
+DO NOT SET VALUE MANUALLY.")
+
+(defvar twm-request-wget-program nil
+  "Cache a result of `twm-request-find-wget-program'.
+DO NOT SET VALUE MANUALLY.")
+
+(defcustom twm-request-tls-program nil
+  "*List of strings containing commands to start TLS stream to a host.
+Each entry in the list is tried until a connection is successful.
+%h is replaced with server hostname, %p with port to connect to.
+Also see `tls-program'.
+If nil, this is initialized with a list of valied entries extracted from
+`tls-program'."
+  :type '(repeat string)
+  :group 'twm-request-mode)
+
+;;;;
+;;;; Basic HTTP functions with curl
+;;;;
+
+(defun twm-request-find-curl-program ()
+  "Returns an appropriate `curl' program pathname or nil if not found."
+  (or (executable-find "curl")
+      (let ((windows-p (memq system-type '(windows-nt cygwin)))
+	    (curl.exe
+	     (expand-file-name
+	      "curl.exe"
+	      (expand-file-name
+	       "win-curl"
+	       (file-name-directory (symbol-file 'twit))))))
+	(and windows-p
+	     (file-exists-p curl.exe) curl.exe))))
+
+(defun twm-request-start-http-session-curl-p ()
+  "Return t if curl was installed, otherwise nil."
+  (unless twm-request-curl-program
+    (setq twm-request-curl-program (twm-request-find-curl-program)))
+  (not (null twm-request-curl-program)))
+
+(defun twm-request-start-http-session-curl-https-p ()
+  "Return t if curl was installed and the curl support HTTPS, otherwise nil."
+  (when (twm-request-start-http-session-curl-p)
+    (unless twm-request-curl-program-https-capability
+      (with-temp-buffer
+	(let ((coding-system-for-read 'iso-safe)
+	      (coding-system-for-write 'iso-safe)
+	      ;; Bind `default-directory' to the temporary directory
+	      ;; because it is possible that the directory pointed by
+	      ;; `default-directory' has been already removed.
+	      (default-directory temporary-file-directory))
+	  (call-process twm-request-curl-program
+			nil (current-buffer) nil
+			"--version")
+	  (goto-char (point-min))
+	  (setq twm-request-curl-program-https-capability
+		(if (search-forward-regexp "^Protocols: .*https" nil t)
+		    'capable
+		  'incapable)))))
+    (eq twm-request-curl-program-https-capability 'capable)))
+
+(defun twm-request-send-http-request-curl (name buffer connection-info sentinel)
+  ""
+  (let* ((request (cdr (assq 'request connection-info)))
+	 (method (cdr (assq 'method request)))
+	 (uri (cdr (assq 'uri request)))
+	 (header-list (cdr (assq 'header-list request)))
+	 (post-body (cdr (assq 'post-body request)))
+	 (use-proxy (cdr (assq 'use-proxy connection-info)))
+	 (proxy-server (cdr (assq 'proxy-server connection-info)))
+	 (proxy-port (cdr (assq 'proxy-port connection-info)))
+	 (proxy-user (cdr (assq 'proxy-user connection-info)))
+	 (proxy-password (cdr (assq 'proxy-password connection-info)))
+	 (use-ssl (cdr (assq 'use-ssl connection-info)))
+	 (allow-insecure-server-cert
+	  (cdr (assq 'allow-insecure-server-cert connection-info)))
+	 (cacert-file-fullpath
+	  (cdr (assq 'cacert-file-fullpath connection-info)))
+	 (cacert-file-base-directory
+	  (when cacert-file-fullpath
+	    (file-name-directory cacert-file-fullpath)))
+	 (cacert-file-body
+	  (when cacert-file-fullpath
+	    (file-name-nondirectory cacert-file-fullpath)))
+	 (header-list
+	  `(,@header-list
+	    ;; Make `curl' remove the HTTP header field "Expect" for
+	    ;; avoiding '417 Expectation Failed' HTTP response error.
+	    ;; The header field is automatically added for a HTTP request
+	    ;; exceeding 1024 byte. See
+	    ;; http://d.hatena.ne.jp/imait/20091228/1262004813 and
+	    ;; http://www.escafrace.co.jp/blog/09/10/16/1008
+	    ("Expect" . "")))
+	 (curl-args
+	  `("--include" "--silent" "--compressed"
+	    ,@(apply 'append
+		     (mapcar
+		      (lambda (pair)
+			;; Do not overwrite internal headers `curl' would use.
+			;; Thanks to William Xu.
+			;; "cURL - How To Use"
+			;; http://curl.haxx.se/docs/manpage.html
+			(unless (string= (car pair) "Host")
+			  `("-H" ,(format "%s: %s" (car pair) (cdr pair)))))
+		      header-list))
+	    ,@(when use-ssl `("--cacert" ,cacert-file-body))
+	    ,@(when (and use-ssl allow-insecure-server-cert)
+		`("--insecure"))
+	    ,@(when (and use-proxy proxy-server proxy-port)
+		(append
+		 `("-x" ,(format "%s:%s" proxy-server proxy-port))
+		 (when (and proxy-user proxy-password)
+		   `("-U" ,(format "%s:%s" proxy-user proxy-password)))))
+	    ,@(when (string= "POST" method)
+		`("-d" ,(or post-body "")))
+	    ,uri))
+	 (coding-system-for-read 'binary)
+	 (coding-system-for-write 'binary)
+	 (default-directory
+	   ;; If `use-ssl' is non-nil, the `curl' process
+	   ;; is executed at the same directory as the temporary cert file.
+	   ;; Without changing directory, `curl' misses the cert file if
+	   ;; you use Emacs on Cygwin because the path on Emacs differs
+	   ;; from Windows.
+	   ;; With changing directory, `curl' on Windows can find the cert
+	   ;; file if you use Emacs on Cygwin.
+	   (if use-ssl
+	       cacert-file-base-directory
+	     default-directory)))
+    (twm-request-start-process-with-sentinel name buffer
+					    twm-request-curl-program
+					    curl-args sentinel)))
+
+(defun twm-request-pre-process-buffer-curl (proc buffer connection-info)
+  ""
+  (let ((use-ssl (cdr (assq 'use-ssl connection-info)))
+	(use-proxy (cdr (assq 'use-proxy connection-info))))
+    (when (and use-ssl use-proxy)
+      ;; When using SSL via a proxy with CONNECT method,
+      ;; omit a successful HTTP response and headers if they seem to be
+      ;; sent from the proxy.
+      (with-current-buffer buffer
+	(save-excursion
+	  (goto-char (point-min))
+	  (let ((first-regexp
+		 ;; successful HTTP response
+		 "\\`HTTP/1\.[01] 2[0-9][0-9] .*?\r?\n")
+		(next-regexp
+		 ;; following HTTP response
+		 "^\\(\r?\n\\)HTTP/1\.[01] [0-9][0-9][0-9] .*?\r?\n"))
+	    (when (and (search-forward-regexp first-regexp nil t)
+		       (search-forward-regexp next-regexp nil t))
+	      (let ((beg (point-min))
+		    (end (match-end 1)))
+		(delete-region beg end)))))))))
+
+;;;;
+;;;; Basic HTTP functions with wget
+;;;;
+
+(defun twm-request-find-wget-program ()
+  "Returns an appropriate `wget' program pathname or nil if not found."
+  (executable-find "wget"))
+
+(defun twm-request-start-http-session-wget-p ()
+  "Return t if `wget' was installed, otherwise nil."
+  (unless twm-request-wget-program
+    (setq twm-request-wget-program (twm-request-find-wget-program)))
+  (not (null twm-request-wget-program)))
+
+(defun twm-request-send-http-request-wget (name buffer connection-info sentinel)
+  ""
+  (let* ((request (cdr (assq 'request connection-info)))
+	 (method (cdr (assq 'method request)))
+	 (scheme (cdr (assq 'scheme request)))
+	 (uri (cdr (assq 'uri request)))
+	 (header-list (cdr (assq 'header-list request)))
+	 (post-body (cdr (assq 'post-body request)))
+	 (use-proxy (cdr (assq 'use-proxy connection-info)))
+	 (proxy-server (cdr (assq 'proxy-server connection-info)))
+	 (proxy-port (cdr (assq 'proxy-port connection-info)))
+	 (proxy-user (cdr (assq 'proxy-user connection-info)))
+	 (proxy-password (cdr (assq 'proxy-password connection-info)))
+	 (use-ssl (cdr (assq 'use-ssl connection-info)))
+	 (allow-insecure-server-cert
+	  (cdr (assq 'allow-insecure-server-cert connection-info)))
+	 (cacert-file-fullpath
+	  (cdr (assq 'cacert-file-fullpath connection-info)))
+	 (cacert-file-base-directory
+	  (when cacert-file-fullpath
+	    (file-name-directory cacert-file-fullpath)))
+	 (cacert-file-body
+	  (when cacert-file-fullpath
+	    (file-name-nondirectory cacert-file-fullpath)))
+	 (args
+	  `("--save-headers"
+	    "--quiet"
+	    "--output-document=-"
+	    ,@(remove nil
+		      (mapcar
+		       (lambda (pair)
+			 (unless (string= (car pair) "Host")
+			   (format "--header=%s: %s" (car pair) (cdr pair))))
+		       header-list))
+	    ,@(when use-ssl
+		`(,(format "--ca-certificate=%s" cacert-file-body)))
+	    ,@(when (and use-ssl allow-insecure-server-cert)
+		`("--no-check-certificate"))
+	    ,@(cond
+	       ((not use-proxy)
+		'("--no-proxy"))
+	       ((and use-proxy proxy-server proxy-port
+		     proxy-user proxy-password)
+		`(,(format "--proxy-user=%s" proxy-user)
+		  ,(format "--proxy-password=%s" proxy-password)))
+	       (t
+		nil))
+	    ,@(when (string= "POST" method)
+		`(,(concat "--post-data=" (or post-body ""))))
+	    ,uri))
+	 (coding-system-for-read 'binary)
+	 (coding-system-for-write 'binary)
+	 (default-directory
+	   ;; If `use-ssl' is non-nil, the `wget' process
+	   ;; is executed at the same directory as the temporary cert file.
+	   ;; Without changing directory, `wget' misses the cert file if
+	   ;; you use Emacs on Cygwin because the path on Emacs differs
+	   ;; from Windows.
+	   ;; With changing directory, `wget' on Windows can find the cert
+	   ;; file if you use Emacs on Cygwin.
+	   (if use-ssl
+	       cacert-file-base-directory
+	     default-directory))
+	 (process-environment
+	  `(,@(when (and use-proxy proxy-server proxy-port)
+		`(,(format "%s_proxy=%s://%s:%s/" scheme
+			   scheme proxy-server proxy-port)))
+	    ,@process-environment)))
+    (twm-request-start-process-with-sentinel name buffer
+					    twm-request-wget-program args
+					    sentinel)))
+
+(defun twm-request-pre-process-buffer-wget (proc buffer connection-info)
+  ""
+  (with-current-buffer buffer
+    (save-excursion
+      (goto-char (point-min))
+      (when (search-forward-regexp "\\`[^\n]*?\r\r\n" (point-max) t)
+	;; When `wget.exe' writes HTTP response in text mode,
+	;; CRLF may be converted into CRCRLF.
+	(goto-char (point-min))
+	(while (search-forward "\r\n" nil t)
+	  (replace-match "\n" nil t)))
+      (goto-char (point-max))
+      (when (search-backward-regexp "\nProcess [^\n]* finished\n\\'"
+				    (point-min) t)
+	(replace-match "" nil t))
+      )))
+
+;;;;
+;;;; Basic HTTP functions with url library
+;;;;
+
+(defun twm-request-start-http-session-urllib-p ()
+  "Return t if url library is available, otherwise nil."
+  (require 'url nil t))
+
+(defun twm-request-start-http-session-urllib-https-p ()
+  "Return t if url library can be used for HTTPS, otherwise nil."
+  (and (not twm-request-proxy-use)
+       (require 'url nil t)
+       (cond
+	((<= 22 emacs-major-version)
+	 ;; On Emacs22 and later, `url' requires `tls'.
+	 (twm-request-start-http-session-native-tls-p))
+	((require 'ssl nil t)
+	 ;; On Emacs21, `url' requires `ssl'.
+	 t)
+	((or (and (fboundp 'open-ssl-stream)
+		  ;; Since `url-gw' (required by `url') defines autoload of
+		  ;; `open-ssl-stream' from "ssl",
+		  ;; (fboundp 'open-ssl-stream) will be non-nil even if
+		  ;; "ssl" cannot be loaded and `open-ssl-stream' is
+		  ;; unavailable.
+		  ;; Here, the availability is confirmed by `documentation'.
+		  (documentation 'open-ssl-stream))
+	     ;; On Emacs21, `url' requires `ssl' in order to use
+	     ;; `open-ssl-stream', which is included in `ssl.el'.
+	     ;; Even if `ssl' cannot be loaded, `open-tls-stream' can be
+	     ;; used as an alternative of the function.
+	     (and (twm-request-start-http-session-native-tls-p)
+		  (defalias 'open-ssl-stream 'open-tls-stream)))
+	 (provide 'ssl)
+	 t)
+	(t
+	 nil))))
+
+(defun twm-request-send-http-request-urllib (name buffer connection-info sentinel)
+  ""
+  (let* ((request (cdr (assq 'request connection-info)))
+	 (method (cdr (assq 'method request)))
+	 (scheme (cdr (assq 'scheme request)))
+	 (uri (cdr (assq 'uri request)))
+	 (header-list (cdr (assq 'header-list request)))
+	 (post-body (cdr (assq 'post-body request)))
+	 (use-proxy (cdr (assq 'use-proxy connection-info)))
+	 (proxy-server (cdr (assq 'proxy-server connection-info)))
+	 (proxy-port (cdr (assq 'proxy-port connection-info)))
+	 (proxy-user (cdr (assq 'proxy-user connection-info)))
+	 (proxy-password (cdr (assq 'proxy-password connection-info)))
+	 (proxy-credentials
+	  (when (and proxy-user proxy-password)
+	    (concat "Basic "
+		    (base64-encode-string
+		     (concat proxy-user ":" proxy-password)))))
+	 (coding-system-for-read 'binary)
+	 (coding-system-for-write 'binary)
+	 (url-proxy-services
+	  (when use-proxy
+	    `((,scheme . ,(format "%s:%s" proxy-server proxy-port)))))
+	 (url-request-method method)
+	 (url-request-extra-headers
+	  ;; Remove some headers that should be configured by url library.
+	  ;; They may break redirections by url library because
+	  ;; `url-request-extra-headers' overwrites the new headers
+	  ;; that are adapted to redirected connection.
+	  (apply 'append
+		 (mapcar (lambda (pair)
+			   (if (member (car pair)
+				       '("Host" "Content-Length"))
+			       nil
+			     `(,pair)))
+			 (if proxy-credentials
+			     (cons
+			      `("Proxy-Authorization" ,proxy-credentials)
+			      header-list)
+			   header-list))))
+	 (url-request-data post-body)
+	 (url-show-status twm-request-url-show-status)
+	 (url-http-attempt-keepalives nil)
+	 (tls-program twm-request-tls-program)
+	 (coding-system-for-read 'binary)
+	 (coding-system-for-write 'binary))
+    (lexical-let ((sentinel sentinel)
+		  (buffer buffer))
+      (let ((result-buffer
+	     (url-retrieve
+	      uri
+	      (lambda (&rest args)
+		(let ((proc url-http-process)
+		      (url-buffer (current-buffer))
+		      (status-str
+		       (if (and (< emacs-major-version 22)
+				(boundp 'url-http-end-of-headers)
+				url-http-end-of-headers)
+			   "urllib-finished"
+			 "finished")))
+		  ;; Callback may be called multiple times.
+		  ;; (as filter and sentinel?)
+		  (unless (local-variable-if-set-p 'twm-request-retrieved)
+		    (set (make-local-variable 'twm-request-retrieved)
+			 'not-completed)
+		    (with-current-buffer buffer
+		      (set-buffer-multibyte nil)
+		      (insert-buffer-substring url-buffer))
+		    (set-process-buffer proc buffer)
+		    (unwind-protect
+			(apply sentinel proc status-str nil)
+		      (set-process-buffer proc url-buffer)
+		      (if (eq twm-request-retrieved 'exited)
+			  (url-mark-buffer-as-dead url-buffer)
+			(setq twm-request-retrieved 'completed))))
+		  (when (memq (process-status proc)
+			      '(nil closed exit failed signal))
+		    ;; Mark `url-buffer' as dead when the process exited
+		    ;; and `sentinel' is completed.
+		    ;; If this `lambda' is evaluated via a filter, the
+		    ;; process may exit before it is finished to evaluate
+		    ;; `(apply sentinel ...)'. In the case, `buffer' should
+		    ;; not be killed. It should be killed after the
+		    ;; evaluation of `sentinel'.
+		    (if (eq twm-request-retrieved 'completed)
+			(url-mark-buffer-as-dead url-buffer)
+		      (setq twm-request-retrieved 'exited))))))))
+	(when (buffer-live-p result-buffer)
+	  (with-current-buffer result-buffer
+	    (set (make-local-variable 'url-show-status)
+		 twm-request-url-show-status)
+	    ;; Make `url-http-attempt-keepalives' buffer-local
+	    ;; in order to send the current value of the variable
+	    ;; to the sentinel invoked for HTTP redirection,
+	    (make-local-variable 'url-http-attempt-keepalives))
+	  (get-buffer-process result-buffer))))))
+
+(defun twm-request-pre-process-buffer-urllib (proc buffer connection-info)
+  ""
+  (with-current-buffer buffer
+    (save-excursion
+      (goto-char (point-max))
+      (cond
+       ((search-backward-regexp
+	 "- Peer has closed the GNUTLS connection\r?\n\\'"
+	 nil t)
+	(let ((beg (match-beginning 0))
+	      (end (match-end 0)))
+	  (delete-region beg end)))
+       ((search-backward-regexp "closed\r?\n\\'" nil t)
+	(let ((beg (match-beginning 0))
+	      (end (match-end 0)))
+	  (delete-region beg end)))
+       (t nil)))))
+
+(provide 'twm-request-bridge)
+;;; twm-request-bridge.el ends here
